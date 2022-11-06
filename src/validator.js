99c10b85b5c8c0973b00f25fdc5aac9285a43d21
Ariel-Navarro <arielknm@gmail.com>
2022-11-05 19:21:19 -0500

    Agrego un paso al algoritmo y elimino los comentarios

--- src/validator.js
+++ src/validator.js
@@ -1,44 +1,44 @@
 const validator = {
   isValid: function (param) {
-    //  Para que me devuelva un array, split me devuelve separados los elementos de una cadena en un array
-  //  let arrayNumeros = param.split('');
-  //  console.log(arrayNumeros)
-
-
     if (param.length == 0) {
       alert("Ingrese su número de tarjeta")
+      return false
     }
     else {
       //  Para que me devuelva un array, split me devuelve separados los elementos de una cadena en un array
      let arrayNumeros = param.split("");
-     console.log(arrayNumeros)
+    //  console.log(arrayNumeros)
       // Revertimos el orden del array
      arrayNumeros.reverse();
-     console.log(arrayNumeros)
+    //  console.log(arrayNumeros)
 
      // para iterar en el array y lo convierto en número
      let test = arrayNumeros.map((elemento) => {
      return Number(elemento);
      })
-     console.log(test)
+    //  console.log(test)
 
     //  iterando para encontar las posiciones pares y luego multiplicar por 2 el elemento
      let findIndex= test.map((elemento,index)=>{
-     if (index % 2 !==0 ){
-      return elemento*2;
+     if (index % 2 !==0){
+      let multiplicando= elemento *=2
+      if (multiplicando > 9) {
+       return multiplicando -=9
+      } else{
+        return multiplicando
+      }
+
      }
      else {
       return elemento
      }
      })
-     console.log(findIndex)
 
     //  .reduce() para sumar todos los números
      const initialValue=0
      let sumaTotal= findIndex.reduce((a,b) =>{
       return a + b
      },initialValue)
-     console.log (sumaTotal)
 
     //  para saber si el total de la suma termina en 0 y al dividirlo entre 10 el residuo es 0
      let resultado =()=>{
@@ -52,58 +52,13 @@ const validator = {
      };
      return resultado();
     }
-
-
-   // // Revertimos el orden del array
-   // arrayNumeros.reverse();
-   // // console.log(arrayNumeros)
-
-   // // para iterar en el array y lo convierto en número
-   // let test = arrayNumeros.map((elemento) => {
-   //   return Number(elemento);
-   // })
-   // // console.log(test)
-
-   // let findIndex= test.map((elemento,index)=>{
-   //   if (index%2 !=0 ){
-   //     return elemento*2;
-   //   }
-   //   else {
-   //     return elemento
-   //   }
-
-   // })
-   // // console.log(findIndex)
-
-   // const initialValue=0
-   // let sumaTotal= findIndex.reduce((a,b) =>{
-   //    return a + b
-   // },initialValue)
-   // // console.log (sumaTotal)
-
-   // let resultado =()=>{
-   //   if (sumaTotal %10===0){
-   //    return true
-   //   }
-   //   else {
-   //     return false
-   //   }
-   // };
-   // return resultado();
   },
 
 
   maskify: function (parametro) {
-    // if (param=" "){
-    // alert("Escriba aquí tu número de tarjeta")
-    // }
-    // else{
       let lastFour= parametro.slice(-4);
-      console.log(lastFour)
       let relleno= lastFour.padStart(parametro.length,"#");
-      console.log(relleno)
       return relleno
-    // }
   }
 };
 
